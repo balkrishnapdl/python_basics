19d6ef2c5b5ad2b1352a2cb5c8fbb2653887ba6b
balkrishnapdl <poudelb52@gmail.com>
2022-11-29 17:01:11 +0545

    First Commit

---
+++ 13 Ifelse.py
@@ -0,0 +1,75 @@
+# a=33
+# b=200
+# if b>a:
+#     print(b,"is greater than ",a)
+
+### ELif
+# a=33
+# b=33
+# if b>a:
+#     print("b is greater than a")
+# elif a==b:
+#     print("a and b are equal")
+
+### Else
+# a=200
+# b=33
+# if b>a:
+#     print(" b is greater than a")
+# elif b==a:
+#     print("a and b are equal")
+# else:
+#     print("a is greater than b")
+
+### Else without the elif
+# a=200
+# b=33
+# if b>a:
+#     print("b is greater than a")
+# else:
+#     print("b isnot greater than a")
+
+### Short Hand if
+# a=200
+# b=33
+# if a>b:print("a is greater than b.")
+
+### Short hand if... Else
+# a=2
+# b=330
+# print("A")if a>b else print("B")
+
+# a=330
+# b=330
+# print("A")if a>b else print("=")if a==b else print("B")
+
+### And keyword
+# a=200
+# b=33
+# c=500
+# if a>b and c>a:
+#     print("Both conditions are satisfied")
+# else:
+#     print("Both conditions arenot satisfied.")
+
+### Or keyword
+# a=200
+# b=33
+# c=500
+# if a>b or a>c:
+#     print("At least one of the conditions is True.")
+
+### Nested  if
+# x=int(input("Guess any number:"))
+# if x>10:
+#     print("Above ten,")
+#     if x>20:
+#         print("Above twenty,")
+#     else:
+#         print("but not above 20.")
+
+### The pass statement
+# a=33
+# b=200
+# if b>a:
+#     pass
---
+++ 14 While loop.py
@@ -0,0 +1,30 @@
+# i=1
+# while i<6:
+#     print(i)
+#     i=i+1
+
+### The break statement.
+# i=1
+# while i<6:
+#     print(i)
+#     if i==3:
+#         break
+#     i=i+1
+
+### The continue statement
+# i=0
+# while i<6:
+#     i=i+1
+#     if i==3:
+#         continue
+#     print(i)
+    
+### The else statement
+# i=1
+# while i<6:
+#     print(i)
+#     i=i+1
+# else:
+#     print("i is no longer less than 6")
+
+
